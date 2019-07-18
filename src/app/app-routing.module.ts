1be9b931435276e25ef21f483995ef2519d4d4d6
ray100 <jraymonddoirin@gmail.com>
2019-07-17 22:22:27 -0400

    auth login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: '',      component: HomeComponent },
-  { path: 'propos',      component: AboutComponent }
+  { path: 'propos',      component: AboutComponent },
+  { path: 'login',      component: LoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { MasterComponent } from './master/master.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    MasterComponent
+    MasterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
