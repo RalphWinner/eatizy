84cc3425c2e5d35f4075781969175a3744f32fc6
Clost <CD042959@digicelgroup.com>
2019-07-18 09:24:14 -0400

    New

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   { path: '',      component: HomeComponent },
   { path: 'propos',      component: AboutComponent },
-  { path: 'login',      component: LoginComponent }
+  { path: 'login',      component: LoginComponent },
+  { path: 'register',      component: RegisterComponent }
 ];
 
 @NgModule({
